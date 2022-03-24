92eefbad5ef87cb43fc1d1aba3e5e44f3b37a601
will <wbrycejohnson031086@gmail.com>
2022-03-24 11:52:29 -0400

    add will remove shaz

--- src/App.js
+++ src/App.js
@@ -2,50 +2,17 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import ManliestThings from './pages/ManliestThings/ManliestThings'
-import FunctionalThings from './pages/FunctionalThings/FunctionalThings'
+import BoardGameThings from './pages/BoardGameThings/BoardGameThings'
 import StyledThings from './pages/StyledThings/StyledThings'
 import SillyThings from './pages/SillyThings/SIllyThings'
 import Landing from './pages/Landing/Landing'
 
 const App = () => {
-  const [shahzadsThings, setShazadsThings] = useState([
+  const [willsThings, setWillsThings] = useState([
     {
-      name: 'functional programming',
-      image: 'https://imgs.xkcd.com/comics/functional.png',
-      attributes: ['efficient', 'reusability', 'not a taco', 'beautiful'],
-    },
-    {
-      name: 'React hooks',
-      image:
-        'https://images.unsplash.com/photo-1518644730709-0835105d9daa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-      attributes: [
-        'useEffect',
-        'useState',
-        'not a taco',
-        "state's best friend",
-      ],
-    },
-    {
-      name: 'BluBlockers',
-      image:
-        'https://susanmaryetta.files.wordpress.com/2011/06/blublocker-the-hangover.png?w=584',
-      attributes: [
-        'very attractive',
-        'useful probably!',
-        'good for viewing tacos',
-        'such hip',
-        'worn by all the cool kids',
-      ],
-    },
-    {
-      name: 'Healthy food',
-      image: 'https://i.imgur.com/gRxOxsA.jpg',
-      attributes: [
-        'makes you live a long time',
-        'consistent poops',
-        'probably tacos sometimes!',
-        'farm to market',
-      ],
+      name: 'board games',
+      image: 'https://i.imgur.com/67na6Bt.jpeg',
+      attributes: ['classic', 'fun', 'spooky', 'replayable', 'not a taco'],
     },
   ])
 
@@ -134,8 +101,8 @@ const App = () => {
       {/* All the <Route> components should live here */}
       <Route path="/" element={<Landing />} />
       <Route
-        path="/the-functional-things"
-        element={<FunctionalThings things={shahzadsThings} />}
+        path="/the-board-game-things"
+        element={<BoardGameThings things={willsThings} />}
       />
       <Route
         path="/the-manliest-things"
